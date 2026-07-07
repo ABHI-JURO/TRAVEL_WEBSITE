45e70b55427e010f3f21ca113b8cc49fbfd5009f
Abhishek Shrivastava <138434193+ABHI-JURO@users.noreply.github.com>
2026-07-07 22:47:28 +0530

    changed height and width

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -4,7 +4,7 @@ const Guide = () => {
     return (
         <section className='flexCenter flex-col'>
             <div className='border-2 border-red-600 padding-container max-container w-full pb-24'>
-                <Image src="/camp.svg" alt="camp" width={50} height={50} />
+                <Image src="/camp.svg" alt="camp" width={51} height={51} />
                 <p className='uppercase regular-18 -mt-1 mb-3 text-green-50'>
                     we are here for you
                 </p>
