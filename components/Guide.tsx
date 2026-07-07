2d23d5c9d2796de90b916f2425bf354294a0f870
Abhishek Shrivastava <138434193+ABHI-JURO@users.noreply.github.com>
2026-07-07 22:41:28 +0530

    started working on guide section in home page

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import Image from 'next/image'
 
 const Guide = () => {
     return (
-        <div>Guide</div>
+        <section className='flexCenter flex-col'>
+            <div className='border-2 border-red-500 padding-container max-container w-full pb-24'>
+                <Image src="/camp.svg" alt="camp" width={50} height={50} />
+                <p className='uppercase regular-18 -mt-1 mb-3 text-green-50'>
+                    we are here for you
+                </p>
+                <div className='flex flex-wrap justify-between gap-5 lg:gap-10'>
+                    <h2>Guide You To Easy Path</h2>
+                    <p>
+                        Only
+                    </p>
+                </div>
+            </div>
+        </section>
     )
 }
 
